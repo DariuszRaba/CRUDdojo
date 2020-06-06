885293d352a248d18dc65db52c834d051cfd99ea
Dariusz Raba <rabadariusz@gmail.com>
2020-06-06 15:57:45 +0200

    Repair server

--- src/main/java/StudentController.java
+++ src/main/java/StudentController.java
@@ -1,13 +1,21 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/students")
 public class StudentController extends HttpServlet {
 
-    public void handle(HttpRequest request, HttpResponse response) {
+//    public void handle(HttpRequest request, HttpResponse response) {
+//
+//    }
 
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
     }
 
     private void index() {
