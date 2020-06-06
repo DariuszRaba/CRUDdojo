82a180ae4972bf744d3524b9a2ccd85ddc28acc6
karbudzik <karolinabudzik92@gmail.com>
2020-06-06 15:10:50 +0200

    Add student class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,51 @@
 public class Student {
+    private int ID;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(int ID, String firstName, String lastName, int age) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
